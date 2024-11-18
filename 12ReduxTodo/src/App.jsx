e4e1a321c30c52fbd338b3c9c9c13940f98ffb8c
unknown <prathameshpise6@gmail.com>
2024-11-18 15:37:28 +0530

    redux

---
+++ 12ReduxTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>thelite redux</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
